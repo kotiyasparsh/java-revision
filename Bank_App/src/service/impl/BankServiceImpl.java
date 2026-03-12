f40c3e879f183e55d914e7a02c53b0e0fe5a2486
Sparsh Kotiya <kotiyasparsh@gmail.com>
2026-03-12 23:45:26 +0530

    bug fixed

--- Bank_App/src/service/impl/BankServiceImpl.java
+++ Bank_App/src/service/impl/BankServiceImpl.java
@@ -4,78 +4,38 @@ import domain.Account;
 import repository.AccountRepository;
 import service.Bankservice;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class BankServiceImpl implements Bankservice {
 
+    private final AccountRepository accountRepository = new AccountRepository();
 
     @Override
     public String openAccount(String name, String email, String accountType) {
 
-
-
-        public class BankServiceImpl implements Bankservice {
-
-
-            @Override
-            public String openAccount(String name, String email, String accountType) {
-
-
-
-
-                 AccountRepository accountRepository = new AccountRepository();
-
-                String customerId = UUID.randomUUID().toString();
-
-                String accountNumber = getAccountNumber(accountRepository);
-                Account account = new Account(accountNumber, customerId, 0.0, accountType);
-
-                accountRepository.save(account);
-                return accountNumber;
-            }
-
-            @Override
-            public List<Account> listAccounts() {
-                return accountRepository.findAll();
-            }
-
-            private static String getAccountNumber(AccountRepository accountRepository) {
-                int size = accountRepository.findAll().size() +1;
-                String accountNumber = String.format("AC%06d",size);
-                return accountNumber;
-
-//        in line --> return String.format("AC%06d",size);
-            }
-
-
-        }
-
-
-
-         AccountRepository accountRepository = new AccountRepository();
-
         String customerId = UUID.randomUUID().toString();
 
-        String accountNumber = getAccountNumber(accountRepository);
+        String accountNumber = getAccountNumber();
         Account account = new Account(accountNumber, customerId, 0.0, accountType);
 
         accountRepository.save(account);
+
         return accountNumber;
     }
 
     @Override
     public List<Account> listAccounts() {
-        return accountRepository.findAll();
+        return accountRepository.findAll().stream()
+                .sorted(Comparator.comparing(Account::getAccountNumber))
+                .collect(Collectors.toList());
+
     }
 
-    private static String getAccountNumber(AccountRepository accountRepository) {
-        int size = accountRepository.findAll().size() +1;
-        String accountNumber = String.format("AC%06d",size);
-        return accountNumber;
-
-//        in line --> return String.format("AC%06d",size);
+    private String getAccountNumber() {
+        int size = accountRepository.findAll().size() + 1;
+        return String.format("AC%06d", size);
     }
-
-
 }
